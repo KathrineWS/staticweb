383ee45a0353d69b0d88f2ee3b7c28ad2bd8e46e
Kathrine <kath415a@stud.kea.dk>
2022-08-26 13:24:16 +0200

    brand info added to js

--- js/product.js
+++ js/product.js
@@ -14,4 +14,11 @@ function showProduct(product) {
     "img.productimage"
   ).src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
   document.querySelector("img.productimage").alt = product.productdisplayname;
+
+  document.querySelector(".sectioninfo .brandbio").textContent =
+    product.brandbio;
+  document.querySelector(".sectioninfo .color").textContent =
+    product.basecolour;
+  document.querySelector(".price").textContent = product.price;
+  document.querySelector(".sectioninfo .season").textContent = product.season;
 }

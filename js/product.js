b40d1a794ac376871419f543f62cbb6d124e2a18
Kathrine <kath415a@stud.kea.dk>
2022-08-26 12:49:41 +0200

    moveon

---
+++ js/product.js
@@ -0,0 +1,10 @@
+const url = "https://kea-alt-del.dk/t7/api/products/2801";
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => showProduct(data));
+
+function showProduct(product) {
+  console.log(product);
+  document.querySelector(".brand");
+}

521527a869f398ae8992cca7c537018c71e904f4
Kathrine <kath415a@stud.kea.dk>
2022-08-26 13:08:25 +0200

    js added

--- js/product.js
+++ js/product.js
@@ -6,5 +6,12 @@ fetch(url)
 
 function showProduct(product) {
   console.log(product);
-  document.querySelector(".brand");
+  document.querySelector(".breadcrumbs .brand").textContent = product.brandname;
+  document.querySelector(".productname").textContent =
+    product.productdisplayname;
+
+  document.querySelector(
+    "img.productimage"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
+  document.querySelector("img.productimage").alt = product.productdisplayname;
 }

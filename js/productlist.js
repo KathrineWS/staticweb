eb7290d77b4eeee1eaa4c55d660103f7ff57b98a
Kathrine <kath415a@stud.kea.dk>
2022-08-31 15:04:46 +0200

    new structure on all

--- js/productlist.js
+++ js/productlist.js
@@ -1,4 +1,27 @@
 const url = "https://kea-alt-del.dk/t7/api/products/2801";
 
-//fect(url)
-//.then(res)=> res.json
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => handleProductList(data));
+
+function handleProductList(data) {
+  //console.log(data);
+  data.forEach(showProduct);
+}
+
+function showProduct(product) {}
+
+/* <article class="smallProduct">
+          <img
+            src="https://kea-alt-del.dk/t7/images/webp/1000/2801.webp"
+            alt="Sahara Team India Fanwear Round Neck Jersey"
+          />
+          <h2>Sahara Team India Fanwear Round Neck Jersey</h2>
+          <p>Price</p>
+          <p class="priceItem"><span>Prev.</span>" DKK 895,-"</p>
+          <div class="discount">
+            <p>Now DKK 860,-</p>
+            <p>-34%</p>
+          </div>
+          <a href="productlist.html">Read more</a>
+        </article> */
